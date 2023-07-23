51bfa064ffeaf37c41f9bcb488f584ee645df98c
ipopop <popop@ntymail.com>
2023-07-23 13:58:36 +0200

    add CODEIUM (AI) docstring in code and tests

--- shiftheroes.py
+++ shiftheroes.py
@@ -15,16 +15,40 @@ class ShiftHeroesAPI:
         self.headers = headers
 
     def get_plannings(self):
+        """
+        Retrieves a list of plannings from the server.
+
+        :return: A JSON object containing the plannings.
+        """
         url = f"{self.base_url}/api/v1/plannings"
         response = requests.get(url, headers=self.headers)
         return response.json()
     
     def get_planning_slots(self, planning_id):
+        """
+        Get the planning slots for a given planning ID.
+
+        Parameters:
+            planning_id (int): The ID of the planning.
+
+        Returns:
+            dict: The planning slots in JSON format.
+        """
         url = f"{self.base_url}/api/v1/plannings/{planning_id}/shifts"
         response = requests.get(url, headers=self.headers)
         return response.json()
 
     def reserve_slot(self, planning_id, slot_id):
+        """
+        Reserves a slot by sending a POST request to the API.
+
+        Args:
+            planning_id (int): The ID of the planning.
+            slot_id (int): The ID of the slot to be reserved.
+
+        Returns:
+            Response: The response object returned by the API.
+        """
         url = f"{self.base_url}/api/v1/plannings/{planning_id}/shifts/{slot_id}/reservations"
         response = requests.post(url, headers=self.headers)
         return response
--- test_shiftheroes.py
+++ test_shiftheroes.py
@@ -4,6 +4,18 @@ from shiftheroes import ShiftHeroesAPI
 
 class TestShiftHeroesAPI(unittest.TestCase):
     def setUp(self):
+        """
+        Set up the necessary variables and objects for the test case.
+
+        This function initializes the base URL, headers, and ShiftHeroesAPI object
+        for the test case. It should be called before each test.
+
+        Parameters:
+            self (TestClass): The test case object.
+
+        Returns:
+            None
+        """
         self.base_url = "https://shiftheroes.fr"
         self.headers = {
             'Authorization': 'Bearer secret_bearer_code',
@@ -11,6 +23,18 @@ class TestShiftHeroesAPI(unittest.TestCase):
         self.api = ShiftHeroesAPI(self.base_url, self.headers)
 
     def test_get_plannings(self):
+        """
+        Test the 'get_plannings' function.
+
+        This function is responsible for testing the 'get_plannings' function of the API class.
+        It checks if the function correctly returns the expected response.
+
+        Parameters:
+            self: The current instance of the test class.
+
+        Returns:
+            None
+        """
         expected_response = [{'id': 'X05fB9', 'planning_type': 'daily', 'state': 'available', 'published_at': '2023-07-22T16:00:28.724Z'}, {'id': 'VezfK6', 'planning_type': 'permanent', 'state': 'available', 'published_at': '2023-07-21T16:35:51.522Z'}, {'id': 'O1oflz', 'planning_type': 'weekly', 'state': 'available', 'published_at': '2023-07-20T13:40:21.779Z'}]
         
         with patch('requests.get') as mock_get:
@@ -20,6 +44,15 @@ class TestShiftHeroesAPI(unittest.TestCase):
         self.assertEqual(response, expected_response)
 
     def test_get_planning_slots(self):
+        """
+        Test the get_planning_slots function.
+
+        Parameters:
+            self (TestClassName): The current test class.
+        
+        Returns:
+            None
+        """
         planning_id = 'O1oflz'
         expected_response = [{'id': 'xZGFwL', 'day': 'lundi', 'start_hour': '2000-01-01T08:00:00.000Z', 'end_hour': '2000-01-01T14:00:00.000Z', 'seats': 14, 'seats_taken': 14}]
 
